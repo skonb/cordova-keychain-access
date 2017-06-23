6f47941d92e73d24cc0f22d65dcf900e8b05fdb4
Shunpei Kobayashi <skonb@me.com>
2017-06-24 00:23:09 +0900

    handle error

--- src/ios/KeychainSwift.swift
+++ src/ios/KeychainSwift.swift
@@ -13,19 +13,23 @@
       keychain.accessGroup = (command.arguments[2] as! String)
     }
 
-    keychain.set(command.arguments[0] as! String, forKey: command.arguments[1] as! String)
     var result = ""
-    if command.arguments.count == 3 {
-      result = "Successfully set key \(command.arguments[1]) with value \(command.arguments[0]) on access group \(command.arguments[2])"
-    } else {
-      result = "Successfully set key \(command.arguments[1]) with value \(command.arguments[0])"
+    var status: CDVCommandStatus = CDVCommandStatus_ERROR
+    if(keychain.set(command.arguments[0] as! String, forKey: command.arguments[1] as! String)){
+        if command.arguments.count == 3 {
+            result = "Successfully set key \(command.arguments[1]) with value \(command.arguments[0]) on access group \(command.arguments[2])"
+        } else {
+            result = "Successfully set key \(command.arguments[1]) with value \(command.arguments[0])"
+        }
+        status = CDVCommandStatus_OK
+    }else{
+        result = "Failed to set key \(command.arguments[1]) with value \(command.arguments[0])"
     }
-
     pluginResult = CDVPluginResult(
-      status: CDVCommandStatus_OK,
-      messageAs: result
+        status: status,
+        messageAs: result
     )
-
+ 
     self.commandDelegate!.send(
       pluginResult,
       callbackId: command.callbackId
